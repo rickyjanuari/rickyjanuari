381f2d84819b8ab643ef50dc879a10745be5e72e
Ricky Januari <ricky010193@gmail.com>
2022-03-20 10:36:04 +0700

    change

--- docs/_app/chunks/vue2-8d27bada.js
+++ docs/_app/chunks/vue2-8d27bada.js
@@ -1,11 +1,11 @@
-import{S as l,i as c,s as u,e as k,c as i,a as r,d as p,b as m,g,I as t}from"./vendor-b62d7b9b.js";function v(e){let a,o=`<code class="language-vue"><span class="token comment">&lt;!-- VolumeControl.vue --></span>
-<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span><span class="token punctuation">></span></span>
-  export default &#123;
-    data: () => (&#123;
-      volume: 0
-    &#125;)
-  &#125;
-<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">></span></span>
+import{S as c,i as l,s as u,e as k,c as i,a as r,d as p,b as m,g,I as t}from"./vendor-b62d7b9b.js";function v(o){let a,e=`<code class="language-vue"><span class="token comment">&lt;!-- VolumeControl.vue --></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span><span class="token punctuation">></span></span><span class="token script"><span class="token language-javascript">
+  <span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token punctuation">&#123;</span>
+    <span class="token function-variable function">data</span><span class="token operator">:</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=></span> <span class="token punctuation">(</span><span class="token punctuation">&#123;</span>
+      <span class="token literal-property property">volume</span><span class="token operator">:</span> <span class="token number">0</span>
+    <span class="token punctuation">&#125;</span><span class="token punctuation">)</span>
+  <span class="token punctuation">&#125;</span>
+</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">></span></span>
 
 <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>\u200Btemplate</span><span class="token punctuation">></span></span>
   <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">></span></span>
@@ -21,4 +21,4 @@ import{S as l,i as c,s as u,e as k,c as i,a as r,d as p,b as m,g,I as t}from"./v
       <span class="token attr-name">v-model</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>volume<span class="token punctuation">"</span></span>
     <span class="token punctuation">/></span></span>
   <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">></span></span>
-<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span></code>`;return{c(){a=k("pre"),this.h()},l(n){a=i(n,"PRE",{class:!0});var s=r(a);s.forEach(p),this.h()},h(){m(a,"class","language-vue")},m(n,s){g(n,a,s),a.innerHTML=o},p:t,i:t,o:t,d(n){n&&p(a)}}}class h extends l{constructor(a){super();c(this,a,null,v,u,{})}}export{h as default};
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span></code>`;return{c(){a=k("pre"),this.h()},l(n){a=i(n,"PRE",{class:!0});var s=r(a);s.forEach(p),this.h()},h(){m(a,"class","language-vue")},m(n,s){g(n,a,s),a.innerHTML=e},p:t,i:t,o:t,d(n){n&&p(a)}}}class f extends c{constructor(a){super();l(this,a,null,v,u,{})}}export{f as default};
