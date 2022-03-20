0e69f18eb01bf940d106efff2afc511939281fdc
Ricky Januari <ricky010193@gmail.com>
2022-03-20 08:11:13 +0700

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
   import preprocess from 'svelte-preprocess'
   import autoprefixer from 'autoprefixer'
-  import adapter from '@sveltejs/adapter-static'
+  import adapter from "@sveltejs/adapter-static"
   import { mdsvex } from 'mdsvex'
   import rehypeAutolinkHeadings from 'rehype-autolink-headings'
   import rehypeSlug from 'rehype-slug'
-
+  const dev = "production" === "development";
   /** @type {import('@sveltejs/kit').Config} */
   const config = {
       // Consult https://github.com/sveltejs/svelte-preprocess
@@ -36,7 +36,14 @@
           // target: '#svelte',
 
           //Added for static adapter
-          adapter: adapter()
+          adapter: adapter({
+              pages: "docs",
+              assets: "docs"
+          }),
+          paths: {
+              // change below to your repo name
+              base: dev ? "" : "/rickyjanuari",
+          },
       }
   }
 
