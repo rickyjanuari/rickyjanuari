a3b3925f21a8d6306a1e27e6de1f455a414d01d4
Ricky Januari <ricky010193@gmail.com>
2022-03-20 07:49:56 +0700

    change

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
   import preprocess from 'svelte-preprocess'
   import autoprefixer from 'autoprefixer'
-  import adapter from '@sveltejs/adapter-auto'
+  import adapter from '@sveltejs/adapter-static'
   import { mdsvex } from 'mdsvex'
   import rehypeAutolinkHeadings from 'rehype-autolink-headings'
   import rehypeSlug from 'rehype-slug'
